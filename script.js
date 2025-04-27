6b0a415f4db00d16f58b76791f4fd4e374065eda
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2025-04-27 12:43:15 +0300

    Update 27/04/2025 12:43

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ function hideSidebar () {
 }   
 
 // DarkMode
-let darkmode = localStorage.getItem("darkmode")
-const themeSwitch = document.getElementById("theme-switch")
+// let darkmode = localStorage.getItem("darkmode")
+// const themeSwitch = document.getElementById("theme-switch")
 
-const enableDarkmode = () => {
-    document.body.classList.add("darkmode")
-    localStorage.setItem("darkmode", "active")
-    document.getElementById("logo").src="Imgs/logo-white.png";
-}
+// const enableDarkmode = () => {
+//     document.body.classList.add("darkmode")
+//     localStorage.setItem("darkmode", "active")
+//     document.getElementById("logo").src="Imgs/logo-white.png";
+// }
 
-const disableDarkmode = () => {
-    document.body.classList.remove("darkmode")
-    localStorage.setItem("darkmode", null)
-    document.getElementById("logo").src="Imgs/logo-blue.png";
-}
+// const disableDarkmode = () => {
+//     document.body.classList.remove("darkmode")
+//     localStorage.setItem("darkmode", null)
+//     document.getElementById("logo").src="Imgs/logo-blue.png";
+// }
 
-if (darkmode ==="active") enableDarkmode()
-    else disableDarkmode()
+// if (darkmode ==="active") enableDarkmode()
+//     else disableDarkmode()
 
-themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem("darkmode")
+// themeSwitch.addEventListener("click", () => {
+//     darkmode = localStorage.getItem("darkmode")
 
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-    darkmode === "active" ? document.getElementById("logo").src="Imgs/logo-blue.png" : document.getElementById("logo").src="../Imgs/logo-white.png";
-})
+//     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+//     darkmode === "active" ? document.getElementById("logo").src="Imgs/logo-blue.png" : document.getElementById("logo").src="../Imgs/logo-white.png";
+// })
